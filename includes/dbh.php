5d8e85a56b0ee686b833869dc6fa09da19c5f7d3
Pavel Hlavsa <148801290+paavlsaurus@users.noreply.github.com>
2023-10-23 19:19:12 +0200

    Update dbh.php

--- includes/dbh.php
+++ includes/dbh.php
@@ -1,9 +1,13 @@
 <?php
 
 
-$dsn = "mysql:host=localhost;dbname=mewxkcxn_database";
-$dbusername = "mewxkcxn_database";
-$dbpassword = "SuperZajímavýHeslo";
+// $dsn = "mysql:host=localhost;dbname=mewxkcxn_database";
+// $dbusername = "mewxkcxn_database";
+// $dbpassword = "SuperZajímavýHeslo";
+
+$dsn = "mysql:host=localhost;dbname=database";
+$dbusername = "root";
+$dbpassword = "";
 
 try{
     $pdo = new PDO($dsn,$dbusername,$dbpassword); 
